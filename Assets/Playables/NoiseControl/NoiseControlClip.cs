1b312e43bd38b97f998ec722c2baedbd64f1daa7
Wolf Van Herreweghe <Wolf@exiin.com>
2019-10-20 01:39:42 +0200

    rename template to Settings

--- Assets/Playables/NoiseControl/NoiseControlClip.cs
+++ Assets/Playables/NoiseControl/NoiseControlClip.cs
@@ -6,11 +6,11 @@ namespace DefaultNamespace
 {
     public class NoiseControlClip : PlayableAsset, ITimelineClipAsset
     {
-        [SerializeField] private NoiseControlBehaviour template = new NoiseControlBehaviour();
+        [SerializeField] private NoiseControlBehaviour settings = new NoiseControlBehaviour();
         
         public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
         {
-            return ScriptPlayable<NoiseControlBehaviour>.Create(graph, template);
+            return ScriptPlayable<NoiseControlBehaviour>.Create(graph, settings);
         }
 
         public ClipCaps clipCaps => ClipCaps.Blending;
